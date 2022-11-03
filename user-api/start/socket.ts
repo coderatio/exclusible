b60c101014648ce5181974944fc55ace49857d12
Josiah Yahaya <dmcbist@gmail.com>
2022-11-03 13:50:10 +0100

    Implemented websocket for FE rates streaming

---
+++ user-api/start/socket.ts
@@ -0,0 +1,13 @@
+import WS from 'App/Services/WebSocketService'
+WS.boot()
+
+/**
+ * Listen for incoming socket connections
+ */
+WS.io.on('connection', (socket) => {
+  socket.emit('rates', { BTC_USD: { buy: 100, sell: 80 } })
+
+  socket.on('client_event', (data) => {
+    console.log(data)
+  })
+})
